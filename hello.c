56e08edc5da98f0f5a8d5b8f271b7978ebddc5ae
mypopy <mypopy@gmail.com>
2011-07-01 17:19:35 +0800

    Change the debug info

--- hello.c
+++ hello.c
@@ -3,15 +3,18 @@
  */                                                    
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/sched.h>
 MODULE_LICENSE("Dual BSD/GPL");
 
-static int hello_init(void)
+static int __init hello_init(void)
 {
 	printk(KERN_ALERT "Hello, world\n");
+	printk(KERN_INFO "The process is \"%s\"(pid %i)\n",
+		current->comm, current->pid);
 	return 0;
 }
 
-static void hello_exit(void)
+static void __exit hello_exit(void)
 {
 	printk(KERN_ALERT "Goodbye, cruel world\n");
 }
